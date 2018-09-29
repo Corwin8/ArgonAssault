a37ce615de511e67d3a29fe66ff193a79436298e
Jakub Dockal <jakub.dockal@gmail.com>
2018-09-29 15:35:31 +0200

    CrossPlatformInputManager

--- Assets/Scripts/ShipControls.cs
+++ Assets/Scripts/ShipControls.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class ShipControls : MonoBehaviour {
 
@@ -11,6 +12,7 @@ public class ShipControls : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+		print(horizontalThrow);
 	}
 }
