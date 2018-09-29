2e7ab1a8fc2b0d65eda3ed070203c5dd2d062713
Jakub Dockal <jakub.dockal@gmail.com>
2018-09-29 13:13:52 +0200

    splashscreen script

--- Assets/Scripts/SplashScreen.cs
+++ Assets/Scripts/SplashScreen.cs
@@ -1,16 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SplashScreen : MonoBehaviour {
 
+
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		Invoke("LoadLevel", 2f);
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update()
+	{
+
+	}
+
+	void LoadLevel()
+	{
+		SceneManager.LoadScene(1);
 	}
 }
