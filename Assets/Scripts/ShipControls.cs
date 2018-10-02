9b5de6740c22a050912026efa04e2fdef4a70e69
Jakub Dockal <jakub.dockal@gmail.com>
2018-10-02 08:37:09 +0200

    movement

--- Assets/Scripts/ShipControls.cs
+++ Assets/Scripts/ShipControls.cs
@@ -5,14 +5,23 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class ShipControls : MonoBehaviour {
 
+	[Tooltip("In ms^-1")][Range(0, 8)] [SerializeField] float xSpeed = 2f;
+	[Range(0, 5)] [SerializeField] float maxXOffset = 3f;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-		print(horizontalThrow);
+		float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+
+		float xOffset = xThrow * xSpeed * Time.deltaTime;
+		float rawXPos = transform.localPosition.x + xOffset;
+		float pos = Mathf.Clamp(rawXPos, -maxXOffset, maxXOffset);
+
+		transform.localPosition = new Vector3(pos, transform.localPosition.y, transform.localPosition.z);
 	}
 }
