321c3b244f658632e5b1ac26af014ccd82c19087
Jakub Dockal <jakub.dockal@gmail.com>
2018-10-06 10:40:04 +0200

    enemy explosions

---
+++ Assets/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	[SerializeField] GameObject enemyDeathFX;
+
+	Collider enemyCollider;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		AddNonTriggerBoxCollider();
+	}
+
+	private void AddNonTriggerBoxCollider()
+	{
+		enemyCollider = gameObject.AddComponent<MeshCollider>();
+		enemyCollider.isTrigger = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnParticleCollision(GameObject other)
+	{
+		Instantiate(enemyDeathFX, transform.position, Quaternion.identity);
+		Destroy(gameObject);
+	}
+}
